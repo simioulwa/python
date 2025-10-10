f16516288c2df6afba5c67d6d600690ae250728d
simioulwa <simioulwa@gmail.com>
2025-10-10 19:41:29 +0100

    Add files via upload

---
+++ pyhton folder/Datatype.py
@@ -0,0 +1,9 @@
+# data types in pyhton
+a = 789
+print("the data type of a is ",type(a))
+b = 7865.78
+print("the data type of a is ",type(b))
+c = True
+print("the data type of a is ",type(c))
+d = "python"
+print("the data type of a is ",type(d))
---
+++ pyhton folder/typecasting.py
@@ -0,0 +1,19 @@
+# data types in pyhton
+a = 789
+print("the data type of a is ",type(a))
+b = 7865.78
+print("the data type of a is ",type(b))
+c = True
+print("the data type of a is ",type(c))
+d = "python"
+print("the data type of a is ",type(d))
+a= str(a)
+print(a)
+print("the data type of a is ",type(a))
+b = int(b)
+print(b)
+print("the data type of a is ",type(b))
+c = int(c)
+print(c)
+print("the data type of a is ",type(c))
+
